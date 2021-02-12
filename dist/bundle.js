c8a35f7b477094f4c32f0550e4f53cd8327c6e07
Bryan Lee <bryan.lee.jh@gmail.com>
2021-02-12 16:30:22 +0800

    Convert to functional component

--- dist/bundle.js
+++ dist/bundle.js
@@ -10,28 +10,6 @@
 "use strict";
 
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
 Object.defineProperty(exports, "__esModule", ({
   value: true
 }));
@@ -40,52 +18,35 @@ var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 
 __webpack_require__(/*! ./HeaderBar.scss */ "./src/components/HeaderBar/HeaderBar.scss");
 
-var HeaderBar = /*#__PURE__*/function (_React$Component) {
-  _inherits(HeaderBar, _React$Component);
-
-  var _super = _createSuper(HeaderBar);
-
-  function HeaderBar() {
-    _classCallCheck(this, HeaderBar);
-
-    return _super.apply(this, arguments);
-  }
-
-  _createClass(HeaderBar, [{
-    key: "render",
-    value: function render() {
-      return React.createElement("div", {
-        className: "container"
-      }, React.createElement("div", {
-        className: "logo"
-      }), React.createElement("div", {
-        className: "container_text"
-      }, "Home"), React.createElement("div", {
-        className: "container_text"
-      }, "RPG"), React.createElement("div", {
-        className: "container_text"
-      }, "Arcade"), React.createElement("div", {
-        className: "container_text"
-      }, "Brain Games"), React.createElement("div", null), React.createElement("div", {
-        className: "loginContainer"
-      }, React.createElement("div", {
-        className: "loginColumn"
-      }, React.createElement("button", {
-        className: "loginButton"
-      }, "Login"), React.createElement("div", {
-        className: "loginText"
-      }, "Support")), React.createElement("div", {
-        className: "loginColumn"
-      }, React.createElement("button", {
-        className: "signupButton"
-      }, "Sign Up"), React.createElement("div", {
-        className: "loginText"
-      }, "Contact Us"))));
-    }
-  }]);
-
-  return HeaderBar;
-}(React.Component);
+function HeaderBar() {
+  return React.createElement("div", {
+    className: "container"
+  }, React.createElement("div", {
+    className: "logo"
+  }), React.createElement("div", {
+    className: "container_text"
+  }, "Home"), React.createElement("div", {
+    className: "container_text"
+  }, "RPG"), React.createElement("div", {
+    className: "container_text"
+  }, "Arcade"), React.createElement("div", {
+    className: "container_text"
+  }, "Brain Games"), React.createElement("div", null), React.createElement("div", {
+    className: "loginContainer"
+  }, React.createElement("div", {
+    className: "loginColumn"
+  }, React.createElement("button", {
+    className: "loginButton"
+  }, "Login"), React.createElement("div", {
+    className: "loginText"
+  }, "Support")), React.createElement("div", {
+    className: "loginColumn"
+  }, React.createElement("button", {
+    className: "signupButton"
+  }, "Sign Up"), React.createElement("div", {
+    className: "loginText"
+  }, "Contact Us"))));
+}
 
 exports.default = HeaderBar;
 
--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -4,35 +4,33 @@ import "./HeaderBar.scss";
 interface HeaderBarProps {
 }
 
-export default class HeaderBar extends React.Component<HeaderBarProps> {
-  public render() {
-    return (
-      <div className="container">
-        <div className="logo" />
-        <div className="container_text">
-          Home
+export default function HeaderBar() {
+  return (
+    <div className="container">
+      <div className="logo" />
+      <div className="container_text">
+        Home
+      </div>
+      <div className="container_text">
+        RPG
+      </div>
+      <div className="container_text">
+        Arcade
+      </div>
+      <div className="container_text">
+        Brain Games
+      </div>
+      <div></div>
+      <div className="loginContainer">
+        <div className="loginColumn"> 
+          <button className="loginButton">Login</button>
+          <div className="loginText">Support</div>
         </div>
-        <div className="container_text">
-          RPG
-        </div>
-        <div className="container_text">
-          Arcade
-        </div>
-        <div className="container_text">
-          Brain Games
-        </div>
-        <div></div>
-        <div className="loginContainer">
-          <div className="loginColumn"> 
-            <button className="loginButton">Login</button>
-            <div className="loginText">Support</div>
-          </div>
-          <div className="loginColumn">
-            <button className="signupButton">Sign Up</button>
-            <div className="loginText">Contact Us</div>
-          </div>
+        <div className="loginColumn">
+          <button className="signupButton">Sign Up</button>
+          <div className="loginText">Contact Us</div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
