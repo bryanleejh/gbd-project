0f501508fef697c82ceb9242698e30856634ab47
Bryan Lee <bryan.lee.jh@gmail.com>
2021-02-16 20:31:32 +0800

    Add mockData

---
+++ src/mockData/mock.js
@@ -0,0 +1,35 @@
+export const imageData = [
+  {
+    url: "../../assets/play-win.png",
+  },
+  {
+    url: "../../assets/element-blocks.png",
+  },
+  {
+    url: "../../assets/racer.png",
+  },
+  {
+    url: "../../assets/caesar.png",
+  },
+  {
+    url: "../../assets/cyberpunk.png",
+  },
+  {
+    url: "../../assets/live-game.png",
+  },
+  {
+    url: "../../assets/bike.png",
+  },
+  {
+    url: "../../assets/esports.png",
+  },
+  {
+    url: "../../assets/cleo.png",
+  },
+  {
+    url: "../../assets/soccer.png",
+  },
+  {
+    url: "../../assets/join-league.png",
+  },
+];
